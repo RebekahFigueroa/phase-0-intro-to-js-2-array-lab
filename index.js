ba72916e1b58075db26668b53d8dca0315886526
Rebekah <figueroarebekah@gmail.com>
2022-12-15 13:16:10 -0600

    commit tree 14ccb4fdc10bbcd7b31ec8ec0f25201a6334c5e4

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+  return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+  return cats.shift(name);
+}
+
+function appendCat(name) {
+  const newCats = [...cats, "Broom"];
+  return newCats;
+}
+
+function prependCat(name) {
+  const newCats = ["Arnold", ...cats];
+  return newCats;
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1, cats.length);
+}
